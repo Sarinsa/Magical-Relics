dd7ab4960ca5d52bc3ef6bc401deecf667e6c469
Sarinsa <mikkemusbruker@gmail.com>
2023-11-28 00:28:46 +0100

    Tiny wall pressure plate fix

--- src/main/java/com/sarinsa/magical_relics/common/block/WallPressurePlateBlock.java
+++ src/main/java/com/sarinsa/magical_relics/common/block/WallPressurePlateBlock.java
@@ -3,8 +3,12 @@ package com.sarinsa.magical_relics.common.block;
 import com.sarinsa.magical_relics.common.core.MagicalRelics;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
@@ -14,8 +18,11 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.List;
 
 public class WallPressurePlateBlock extends PressurePlateBlock {
 
@@ -23,18 +30,25 @@ public class WallPressurePlateBlock extends PressurePlateBlock {
 
     // South, west, north, east
     private static final VoxelShape[] shapes = {
-            Block.box(1.0D, 1.0D, 0.0D, 15.0D, 15.0F, 1.0D),
-            Block.box(15.0D, 1.0D, 1.0D, 16.0D, 15.0F, 15.0D),
-            Block.box(1.0D, 1.0D, 15.0D, 15.0D, 15.0F, 16.0D),
-            Block.box(0.0D, 1.0D, 1.0D, 1.0D, 15.0F, 15.0D)
+            Block.box(1.0D, 1.0D, 0.0D, 15.0D, 15.0D, 1.0D),
+            Block.box(15.0D, 1.0D, 1.0D, 16.0D, 15.0D, 15.0D),
+            Block.box(1.0D, 1.0D, 15.0D, 15.0D, 15.0D, 16.0D),
+            Block.box(0.0D, 1.0D, 1.0D, 1.0D, 15.0D, 15.0D)
     };
 
     // South, west, north, east
     private static final VoxelShape[] pressedShapes = {
-            Block.box(1.0D, 1.0D, 0.0D, 15.0D, 15.0F, 1.0D),
-            Block.box(15.0D, 1.0D, 1.0D, 16.0D, 15.0F, 15.0D),
-            Block.box(1.0D, 1.0D, 15.0D, 15.0D, 15.0F, 16.0D),
-            Block.box(0.0D, 1.0D, 1.0D, 1.0D, 15.0F, 15.0D)
+            Block.box(1.0D, 1.0D, 0.0D, 15.0D, 15.0D, 1.0D),
+            Block.box(15.0D, 1.0D, 1.0D, 16.0D, 15.0D, 15.0D),
+            Block.box(1.0D, 1.0D, 15.0D, 15.0D, 15.0D, 16.0D),
+            Block.box(0.0D, 1.0D, 1.0D, 1.0D, 15.0D, 15.0D)
+    };
+
+    private static final AABB[] touchShapes = {
+            new AABB(0.1D, 0.1D, 0.0D, 0.9375D, 0.9375D, 0.1D),
+            new AABB(15.0D, 0.1D, 0.1D, 1.0D, 0.9375D, 0.9375D),
+            new AABB(0.1D, 0.1D, 0.9375D, 0.9375D, 0.9375D, 1.0D),
+            new AABB(0.0D, 0.1D, 0.1D, 0.1D, 0.9375D, 0.9375D)
     };
 
 
@@ -42,6 +56,26 @@ public class WallPressurePlateBlock extends PressurePlateBlock {
     public WallPressurePlateBlock(Properties properties) {
         super(Sensitivity.EVERYTHING, properties.noOcclusion().noCollission());
         registerDefaultState(stateDefinition.any().setValue(POWERED, false).setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected int getSignalStrength(Level level, BlockPos pos) {
+        AABB aabb = touchShapes[level.getBlockState(pos).getValue(FACING).get2DDataValue()].move(pos);
+        List<? extends Entity> list = level.getEntities(null, aabb);
+
+        if (!list.isEmpty()) {
+            for(Entity entity : list) {
+                if (!entity.isIgnoringBlockTriggers()) {
+                    return 15;
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    protected void updateNeighbours(Level level, BlockPos pos) {
+        level.updateNeighborsAt(pos, this);
     }
 
     @Override
