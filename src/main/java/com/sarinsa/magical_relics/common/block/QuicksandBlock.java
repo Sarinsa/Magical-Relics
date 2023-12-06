b094f66eab70cc3ed4f067ab2751aa940999676f
Sarinsa <mikkemusbruker@gmail.com>
2023-12-06 23:51:28 +0100

    Some quicksand stuff

---
+++ src/main/java/com/sarinsa/magical_relics/common/block/QuicksandBlock.java
@@ -0,0 +1,66 @@
+package com.sarinsa.magical_relics.common.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.WebBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class QuicksandBlock extends Block {
+
+    public static final int MAX_HEIGHT = 8;
+    public static final IntegerProperty LAYERS = BlockStateProperties.LAYERS;
+
+    protected static final VoxelShape[] SHAPE_BY_LAYER = new VoxelShape[] {
+            Shapes.empty(),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
+
+
+
+    public QuicksandBlock() {
+        super(BlockBehaviour.Properties.of(Material.DIRT, MaterialColor.RAW_IRON)
+                .strength(0.8F, 1.0F)
+                .noLootTable()
+                .noCollission()
+        );
+        registerDefaultState(stateDefinition.any().setValue(LAYERS, 1));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_LAYER[state.getValue(LAYERS)];
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        entity.makeStuckInBlock(state, new Vec3(0.5D, 0.2D, 0.5D));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder.add(LAYERS));
+    }
+}
