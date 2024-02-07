935a0fd2fdcab78baeb0dd5a2db7c21d19591f07
Sarinsa <mikkemusbruker@gmail.com>
2024-02-07 02:07:52 +0100

    Added todo list

--- src/main/java/com/sarinsa/magical_relics/common/core/MagicalRelics.java
+++ src/main/java/com/sarinsa/magical_relics/common/core/MagicalRelics.java
@@ -16,6 +16,31 @@ import org.apache.logging.log4j.Logger;
 @Mod(MagicalRelics.MODID)
 public class MagicalRelics {
 
+    // TODO LIST
+    //
+    // - Make separate models for Thick Tripwire (consider a slightly different THICCER texture as well?)
+    //
+    // - Make the Display Pedestal block emit a weak redstone signal when the contained item is taken out
+    //
+    // - Make a lock function and key for the Display Pedestal, preventing looting unless a key is found first
+    //
+    // - Rebuild the old wizard towers with a slight makeover and make them generate
+    //
+
+
+    // REMINDERS
+    //
+    // - Investigate the weirdness with Cashout and Air Sneak ability!
+    //
+    // - Look around for a way to allow attribute mods on ItemStacks to be applied
+    //   regardless of what equipment slot the item is in
+    //
+    // - Consider monitoring player health and removing extra hearts when a health boost
+    //   artifact is unequipped
+    //
+
+
+
     public static final String MODID = "magical_relics";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
