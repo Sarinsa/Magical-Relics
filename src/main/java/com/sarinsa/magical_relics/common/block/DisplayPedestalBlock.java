d1d38207fe7a454190b99f76d26cf21334b06340
Sarinsa <mikkemusbruker@gmail.com>
2024-02-08 14:24:44 +0100

    Display Pedestal now emits redstone signal when contained item is taken out

--- src/main/java/com/sarinsa/magical_relics/common/block/DisplayPedestalBlock.java
+++ src/main/java/com/sarinsa/magical_relics/common/block/DisplayPedestalBlock.java
@@ -8,6 +8,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -19,9 +21,12 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.pathfinder.PathComputationType;
@@ -32,9 +37,11 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("deprecation")
 public class DisplayPedestalBlock extends Block implements EntityBlock {
 
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
 
     private static final VoxelShape shape = Shapes.or(Shapes.or(
             Block.box(3.0F, 0.0F, 3.0F, 13.0F, 2.0F, 13.0F),
@@ -47,7 +54,7 @@ public class DisplayPedestalBlock extends Block implements EntityBlock {
                 .strength(1.0F, 0.5F)
                 .sound(SoundType.GLASS));
 
-        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, false));
     }
 
     @Override
@@ -60,6 +67,9 @@ public class DisplayPedestalBlock extends Block implements EntityBlock {
                 if (!displayPedestal.getArtifact().isEmpty()) {
                     Block.popResource(level, pos.above(), displayPedestal.getArtifact());
                     displayPedestal.setArtifact(ItemStack.EMPTY);
+
+                    level.setBlock(pos, state.setValue(POWERED, true), Block.UPDATE_ALL);
+                    level.scheduleTick(pos, this, 10);
 
                     if (!level.isClientSide) {
                         level.playSound(null, pos, SoundEvents.ITEM_FRAME_REMOVE_ITEM, SoundSource.BLOCKS, 0.7F, 1.0F);
@@ -81,6 +91,27 @@ public class DisplayPedestalBlock extends Block implements EntityBlock {
             }
         }
         return super.use(state, level, pos, player, hand, hitResult);
+    }
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(POWERED))
+            level.setBlock(pos, state.setValue(POWERED, false), Block.UPDATE_ALL);
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return state.getValue(POWERED);
+    }
+
+    @Override
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(POWERED) ? 3 : 0;
+    }
+
+    @Override
+    public int getDirectSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return direction != Direction.UP ? state.getSignal(level, pos, direction) : 0;
     }
 
     @Override
@@ -136,6 +167,6 @@ public class DisplayPedestalBlock extends Block implements EntityBlock {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(FACING);
+        stateBuilder.add(FACING, POWERED);
     }
 }
