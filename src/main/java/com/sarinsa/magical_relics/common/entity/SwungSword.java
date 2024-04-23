b4aa9dee62398ce3335893ec6b3f2d65bc187acb
Sarinsa <mikkemusbruker@gmail.com>
2024-04-23 16:24:38 +0200

    ye

---
+++ src/main/java/com/sarinsa/magical_relics/common/entity/SwungSword.java
@@ -0,0 +1,34 @@
+package com.sarinsa.magical_relics.common.entity;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class SwungSword extends Entity {
+
+    public SwungSword(EntityType<?> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+
+    }
+
+    @Override
+    protected void readAdditionalSaveData(CompoundTag compoundTag) {
+
+    }
+
+    @Override
+    protected void addAdditionalSaveData(CompoundTag compoundTag) {
+
+    }
+
+    @Override
+    public Packet<?> getAddEntityPacket() {
+        return null;
+    }
+}
