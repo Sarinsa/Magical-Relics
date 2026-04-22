980f19a9836fefb994b6f1d4b64b88317e23b460
Sarinsa <mikkemusbruker@gmail.com>
2026-04-22 21:00:31 +0200

    Capped Sailor ability's max speed multiplier to 1.0 so boats don't go insaneo mode

--- src/main/java/com/sarinsa/magical_relics/common/ability/SailorAbility.java
+++ src/main/java/com/sarinsa/magical_relics/common/ability/SailorAbility.java
@@ -6,9 +6,11 @@ import com.sarinsa.magical_relics.common.ability.base.BaseArtifactAbility;
 import com.sarinsa.magical_relics.common.ability.base.TriggerType;
 import com.sarinsa.magical_relics.common.core.MagicalRelics;
 import com.sarinsa.magical_relics.common.core.config.ability.AbilityConfig;
+import com.sarinsa.magical_relics.common.core.config.sync.SyncedProperties;
 import fathertoast.crust.api.config.common.AbstractConfigCategory;
 import fathertoast.crust.api.config.common.ConfigManager;
 import fathertoast.crust.api.config.common.field.DoubleField;
+import fathertoast.crust.api.config.common.field.InjectionWrapperField;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -49,30 +51,32 @@ public class SailorAbility extends BaseArtifactAbility<SailorAbility.SailorAbili
         public Sailor SAILOR;
         
         public SailorAbilityConfig( ConfigManager cfgManager, ResourceLocation abilityId,
-                                    int d ) {
+                                    double speedMult ) {
             super( cfgManager, abilityId );
             
-            SAILOR = new Sailor( this );
+            SAILOR = new Sailor( this, speedMult );
         }
         
         public static class Sailor extends AbstractConfigCategory<SailorAbilityConfig> {
             
-            public DoubleField speedMultiplier;
+            public InjectionWrapperField<DoubleField> speedMultiplier;
             
-            public Sailor( SailorAbilityConfig parent ) {
+            public Sailor( SailorAbilityConfig parent, double speedMult ) {
                 super( parent, "sailor", "Options for how this ability affects boat travel." );
                 
-                speedMultiplier = SPEC.define( new DoubleField( "speed_multiplier", 0.05D, 0.0D, 100.0D,
+                speedMultiplier = SPEC.define( new InjectionWrapperField<>( new DoubleField( "speed_multiplier", speedMult, 0.0D, 1.0D,
                         "The row speed multiplier granted by this ability.",
                         "A value of 0.0 would equal 100% of normal boat speed (no change).",
-                        "A value of 0.05 would equal a 5% increase. " ) );
+                        "A value of 0.05 would equal a 5% increase. " ), ( field ) -> {
+                    SyncedProperties.broadcastSyncProperty( SyncedProperties.SAILOR_ABILITY_SPEED_MULT );
+                } ) );
             }
         }
     }
     
     @Override
     public AbilityConfig createConfig( ConfigManager cfgManager, ResourceLocation abilityId ) {
-        return new SailorAbilityConfig( cfgManager, abilityId, 1 );
+        return new SailorAbilityConfig( cfgManager, abilityId, 0.05 );
     }
     
     @Override
