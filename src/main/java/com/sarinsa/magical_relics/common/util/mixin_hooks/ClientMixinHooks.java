d92e07557c9f45102e89eeb35bc7c609762b9c8a
Sarinsa <mikkemusbruker@gmail.com>
2026-04-30 16:33:01 +0200

    Removed MultiPlayerGameModeMixin as it is no longer needed

--- src/main/java/com/sarinsa/magical_relics/common/util/mixin_hooks/ClientMixinHooks.java
+++ src/main/java/com/sarinsa/magical_relics/common/util/mixin_hooks/ClientMixinHooks.java
@@ -2,31 +2,13 @@ package com.sarinsa.magical_relics.common.util.mixin_hooks;
 
 import com.sarinsa.magical_relics.common.core.config.sync.SyncedProperties;
 import com.sarinsa.magical_relics.common.core.registry.MRArtifactAbilities;
-import com.sarinsa.magical_relics.common.tag.MRItemTags;
 import com.sarinsa.magical_relics.common.util.ArtifactUtils;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
 
 public class ClientMixinHooks {
-    
-    public static void onSameDestroyTargetHook( BlockPos pos, BlockPos destroyBlockPos, @Nullable Player player, ItemStack destroyingItem, CallbackInfoReturnable<Boolean> cir ) {
-        if( player == null ) return;
-        
-        final ItemStack itemStack = player.getMainHandItem();
-        
-        if( itemStack.is( MRItemTags.ARTIFACTS ) ) {
-            if( pos.equals( destroyBlockPos ) && !destroyingItem.shouldCauseBlockBreakReset( itemStack ) ) {
-                cir.setReturnValue( true );
-            }
-        }
-    }
     
     public static void onControlBoat( Boat boat, CallbackInfo callbackInfo ) {
         if( boat.getControllingPassenger() instanceof Player player ) {
