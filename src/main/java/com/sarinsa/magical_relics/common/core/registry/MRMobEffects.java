049a50a54e4afcce88d323d4848db42a25d614c9
Sarinsa <mikkemusbruker@gmail.com>
2026-05-07 13:29:52 +0200

    Removed now unused class CloudyVisionEffect

--- src/main/java/com/sarinsa/magical_relics/common/core/registry/MRMobEffects.java
+++ src/main/java/com/sarinsa/magical_relics/common/core/registry/MRMobEffects.java
@@ -1,8 +1,9 @@
 package com.sarinsa.magical_relics.common.core.registry;
 
 import com.sarinsa.magical_relics.common.core.MagicalRelics;
-import com.sarinsa.magical_relics.common.mob_effects.CloudyVisionEffect;
+import com.sarinsa.magical_relics.common.mob_effects.BaseEffect;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,5 +13,5 @@ public class MRMobEffects {
     public static DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create( ForgeRegistries.MOB_EFFECTS, MagicalRelics.MODID );
     
     
-    public static final RegistryObject<MobEffect> CLOUDY_VISION = MOB_EFFECTS.register( "cloudy_vision", CloudyVisionEffect::new );
+    public static final RegistryObject<MobEffect> CLOUDY_VISION = MOB_EFFECTS.register( "cloudy_vision", () -> new BaseEffect( MobEffectCategory.NEUTRAL, 0x9FB5B2 ) );
 }
